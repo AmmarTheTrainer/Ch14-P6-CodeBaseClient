30d642e2db9cefca7a25b002096bfd6118c89ce9
Ammar The Trainer <ammarthetrainer@gmail.com>
2018-11-15 18:36:56 +0500

    Final Update

--- Ch14-P6-CodeBaseClient/Program.cs
+++ Ch14-P6-CodeBaseClient/Program.cs
@@ -4,13 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Ch14_P2_CarLibrary;
+
 namespace Ch14_P6_CodeBaseClient
 {
     class Program
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("***** Fun with CodeBases *****");
+            SportsCar c = new SportsCar();
+            Console.WriteLine("Sports car has been allocated.");
+            Console.ReadLine();
         }
     }
 }
